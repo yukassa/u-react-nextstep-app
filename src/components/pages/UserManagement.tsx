9e573b0f49f7846fa11fd939daec0f6ef50822cc
yukassa <33488471+yukassa@users.noreply.github.com>
2023-01-28 21:59:16 +0900

    Update UserManagement.tsx
    id=10 の場合、ユーザ詳細ページを編集できる機能を追加

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -66,7 +66,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
