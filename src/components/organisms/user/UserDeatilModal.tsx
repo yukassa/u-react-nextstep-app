804cc1f9e127f8bb8c74efc3d8135fec357dbe08
yukassa <33488471+yukassa@users.noreply.github.com>
2023-01-28 22:00:26 +0900

    Update UserDeatilModal.tsx
    id=10 のとき、ユーザ詳細ページを編集できる機能を追加

--- src/components/organisms/user/UserDeatilModal.tsx
+++ src/components/organisms/user/UserDeatilModal.tsx
@@ -1,30 +1,56 @@
 import {
-  Box,
   FormControl,
   FormLabel,
-  Image,
   Input,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Stack,
-  Text
+  Stack
 } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 
 import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 type Props = {
   user: User | null;
   isOpen: boolean;
+  isAdmin?: boolean;
   onClose: () => void;
 };
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const { user, isOpen, onClose } = props;
+  const { user, isOpen, isAdmin = false, onClose } = props;
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    setUsername(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUsername(e.target.value);
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setPhone(e.target.value);
+
+  const onClickUpdate = () => {
+    alert();
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -40,22 +66,43 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>名前</FormLabel>
-              <Input value={user?.username} isReadonly />
+              <Input
+                value={username}
+                onChange={onChangeUserName}
+                isReadonly={!isAdmin}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>フルネーム</FormLabel>
-              <Input value={user?.name} isReadonly />
+              <Input
+                value={name}
+                onChange={onChangeName}
+                isReadonly={!isAdmin}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>MAIL</FormLabel>
-              <Input value={user?.email} isReadonly />
+              <Input
+                value={email}
+                onChange={onChangeEmail}
+                isReadonly={!isAdmin}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>TEL</FormLabel>
-              <Input value={user?.phone} isReadonly />
+              <Input
+                value={phone}
+                onChange={onChangePhone}
+                isReadonly={!isAdmin}
+              />
             </FormControl>
           </Stack>
         </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClickUpdate}>更新</PrimaryButton>
+          </ModalFooter>
+        )}
       </ModalContent>
     </Modal>
   );
